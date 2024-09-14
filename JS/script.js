edfef419017c68a07f6253560e5395755d03fb54
Sparky17561 <studentjamdar@gmail.com>
2024-09-14 22:42:42 +0530

    Started with JS

---
+++ JS/script.js
@@ -0,0 +1,73 @@
+console.log("Hello World")
+
+console.log("Hello World")
+
+
+let a = 10;
+let b = 5;
+console.log(`sum is ${a+b}`);
+
+
+if(a>1){
+    console.log("You are eligible");
+}
+else{
+    console.log("you are not");
+}
+
+
+
+
+let size = ""
+
+if(size == "XL"){
+    console.log("Rs250")
+
+}
+else if (size == "L") {
+    console.log("Rs200")
+} 
+
+else if (size == "M") {
+    console.log("Rs150")
+} 
+
+else if (size == "S") {
+    console.log("Rs100")
+} 
+
+else{
+    console.log("invalid")
+}
+
+
+
+let num =25;
+
+if(num%10==0){
+    console.log("good")
+}
+else{
+    console.log("bad")
+}
+
+
+// let name = prompt("Enter name");
+// let age = prompt("enter age");
+
+// console.log(`${name} is ${age} years old`)
+
+let ch = 1;
+
+switch(ch){
+    case 1: console.log("Jan,Feb,Mar");
+            break;
+
+    case 2: console.log("Apr,May,Jun");
+    break;
+    case 3: console.log("Jul,Aug,Sep");
+    break;
+    case 4: console.log("Oct,Nov,Dec");
+}
+
+
