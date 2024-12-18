627c3267925424a75e3e1cf79737c0c50dc041d8
Sparky17561 <studentjamdar@gmail.com>
2024-12-18 14:02:11 +0530

    finally done with mongo

---
+++ Assets/MONGO3/init.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+const Chat = require('./models/chat.js')
+
+
+main().then(()=>{
+    console.log('connection successful')
+})
+.catch(err => console.log(err))
+
+async function main(){
+    await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
+}
+
+
+
+let allChats = [
+    {
+    from : 'neha',
+    to : 'priya',
+    msg: 'send me ur exam sheets',
+    created_at: new Date(),
+    },
+    {
+        from : 'raju',
+        to : 'priya',
+        msg: 'send me assignments',
+        created_at: new Date(),
+    },
+]
+
+Chat.insertMany(allChats)
