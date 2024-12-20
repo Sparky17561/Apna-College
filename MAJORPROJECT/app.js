446dfcf25bf15935606a23fe3d973c9b2464c397
Sparky17561 <studentjamdar@gmail.com>
2024-12-20 18:02:45 +0530

    phase 1 completed

--- MAJORPROJECT/app.js
+++ MAJORPROJECT/app.js
@@ -6,6 +6,9 @@ const MONGO_URL = 'mongodb://127.0.0.1:27017/wanderlust'
 const path = require('path')
 const methodOverride = require('method-override')
 const ejsMate = require('ejs-mate')
+const wrapAsync = require('./utils/wrapAsync.js')
+const ExpressError = require('./utils/ExpressError.js')
+const {listingSchema} = require('./schema.js')
 
 main().then(()=>{
     console.log('connected to db')
@@ -24,55 +27,60 @@ app.get('/',(req,res)=>{
     res.send('Hi, Im root')
 })
 
+const validateListing = (req,res,next) =>{
+    let {error} = listingSchema.validate(req.body)
+    if(error){
+        let errMsg = error.details.map((el)=>el.message).join(',');
+        throw new ExpressError(400,errMsg)
+    }else{
+        next();
+    }
+}
 //Index route
-app.get('/listings',async (req,res)=>{
+app.get('/listings',  wrapAsync(async (req,res)=>{
     const allListings = await Listing.find({})
     res.render('listings/index.ejs',{allListings})
-    })
+    }))
 //Add route 
 app.get('/listing/new', (req,res)=>{
     res.render('listings/new.ejs')
 })
 //Show route
-app.get('/listings/:id',async (req,res)=>{
+app.get('/listings/:id',  wrapAsync(async (req,res)=>{
     let {id} = req.params
     const listing = await Listing.findById(id)
     res.render('./listings/show.ejs',{listing})
-})
+}))
 // create route 
-app.post('/listings',async (req,res)=>{
+app.post('/listings',validateListing, wrapAsync( async (req,res,next)=>{
     const newListing = new Listing(req.body.Listing)
-    console.log(newListing)
     await newListing.save()
     res.redirect('/listings')
-})
+   
+}))
 //Edit route 
-app.get('/listings/:id/edit', async (req,res)=>{
+app.get('/listings/:id/edit', wrapAsync( async (req,res)=>{
     let {id} = req.params
     const listing = await Listing.findById(id)
     console.log(listing)
     res.render('listings/edit.ejs',{listing})
-})
+}))
 // Update Route 
 // PUT route for updating a listing
-app.put('/listings/:id', async (req, res) => {
+app.put('/listings/:id',validateListing,  wrapAsync(async (req, res) => {
     const { id } = req.params;
-    try {
-        const updatedListing = await Listing.findByIdAndUpdate(id, { ...req.body.Listing }, { new: true });
-        res.redirect(`/listings/${updatedListing._id}`);
-    } catch (err) {
-        console.log('Error updating listing:', err);
-        res.status(500).send('Error updating listing');
-    }
-});
+    const updatedListing = await Listing.findByIdAndUpdate(id, { ...req.body.Listing }, { new: true });
+    res.redirect(`/listings/${updatedListing._id}`);
+
+}))
 
 // delete route 
-app.delete('/listings/:id', async (req,res)=>{
+app.delete('/listings/:id',  wrapAsync(async (req,res)=>{
     let {id} = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id)
     console.log(deletedListing)
     res.redirect('/listings')
-})
+}))
 
 // app.get('/testListing',async (req,res)=>{
 //     let sampleListing = new Listing({
@@ -87,7 +95,15 @@ app.delete('/listings/:id', async (req,res)=>{
 //     res.send('successful')
 // })
 
-
+// standard response , kuch bhi url dia toh ye error aayega 
+app.all('*',(req,res,next)=>{
+    next(new ExpressError(404,'page not found'))
+})
+app.use((err,req,res,next)=>{
+    let {statusCode=500, message='something went wrong'} = err;
+    res.render('error.ejs',{err})
+    // res.status(statusCode).send(message)
+})
 
 app.listen(8080,()=>{
     console.log('server is listening to port 8080......')
